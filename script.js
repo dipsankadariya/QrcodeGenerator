1387cf07df63b89d1a22ddd7ab50b3ca92339461
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-04-30 16:27:37 +0545

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+let qrImage = document.getElementById("qr-code-image");
+let btn = document.getElementById("generate-btn");
+let inputText = document.getElementById("input-text");
+
+function getQr(){
+    let inputValue = inputText.value;
+  qrImage.src =  qrImage.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + inputValue;
+}
+btn.addEventListener("click",()=>{
+    getQr();
+ inputText.value ="";
+});
+
+inputText.addEventListener("keydown",(e)=>{
+    if(e.key === "Enter"){
+        getQr();
+        inputText.value="";
+    }
+});
+
+
